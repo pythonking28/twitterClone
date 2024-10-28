3171d6254e73ad92d572e19a51aaa46ca28fcdeb
pythonking28 <nirajthakur2828@gmail.com>
2024-10-28 20:54:47 +0545

    Link restore api

--- frontend/twitter/src/utils/constants.js
+++ frontend/twitter/src/utils/constants.js
@@ -1,2 +1,4 @@
-export const USER_API_ENDPOINT="http://localhost:8000/api/v1/user" || "https://twitterclone-5sng.onrender.com/"
-export const TWEET_API_ENDPOINT="http://localhost:8000/api/v1/tweet" || "https://twitterclone-5sng.onrender.com/"
+// export const USER_API_ENDPOINT="http://localhost:8000/api/v1/user"
+export const USER_API_ENDPOINT= "https://twitterclone-5sng.onrender.com/"
+// export const TWEET_API_ENDPOINT="http://localhost:8000/api/v1/tweet"
+export const TWEET_API_ENDPOINT= "https://twitterclone-5sng.onrender.com/"
