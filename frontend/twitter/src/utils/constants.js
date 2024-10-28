7e5b9711ea1210b2b04681402ed3326e941aa896
pythonking28 <nirajthakur2828@gmail.com>
2024-10-28 21:25:18 +0545

    Link restore api path

--- frontend/twitter/src/utils/constants.js
+++ frontend/twitter/src/utils/constants.js
@@ -1,4 +1,4 @@
 // export const USER_API_ENDPOINT="http://localhost:8000/api/v1/user"
-export const USER_API_ENDPOINT= "https://twitterclone-5sng.onrender.com"
+export const USER_API_ENDPOINT= "https://twitterclone-5sng.onrender.com/api/v1/user"
 // export const TWEET_API_ENDPOINT="http://localhost:8000/api/v1/tweet"
-export const TWEET_API_ENDPOINT= "https://twitterclone-5sng.onrender.com"
+export const TWEET_API_ENDPOINT= "https://twitterclone-5sng.onrender.com/api/v1/tweet"
